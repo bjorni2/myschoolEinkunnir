a5b90df6ab69b490e0d66bcb523547ef463f738d
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-11-07 20:27:30 +0000

    Added the script, updated readme

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ myschoolEinkunnir
 
 Birtir einkunnir í myschool með html5 canvas
 
-Addon fyrir chrome
-[Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+##Chrome
 
-Addon fyrir firefox
-[Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+Addon: [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+
+Install script: [http://tampermonkey.net/faq.php#Q102]
+
+##Firefox
+
+Addon: [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+
+Install script: ```Ctrl + O``` og velja svo ```myschoolEinkunnir.user.js```
---
+++ myschoolEinkunnir.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name                Myschool einkunnir
+// @namespace	        http://www.ru.is/
+// @description	        Birtir einkunnadreifingu.
+// @include				https://myschool.ru.is/myschool/*verkID*
+// @require				http://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.4.1/canvas.min.js
+// ==/UserScript==
+var myList = document.getElementsByTagName("h3"); // get all h3 elements
+for(var i = 0; i < myList.length; i++){
+    if(myList[i].textContent  == 'Tölfræði'){
+        //alert('Fann tölfræði');
+        var cell = myList[i].nextSibling.firstChild.firstChild.firstChild;
+        var cellContent = cell.innerHTML;
+        //alert(cellContent);
+        var dataIndex = cellContent.indexOf('data');
+        var data = cellContent.substring(dataIndex);
+        data = data.substring(data.indexOf('=') + 1, data.indexOf('&'));
+        //alert(data);
+        data = data.split('|');
+        
+        var legendIndex = cellContent.indexOf('legend');
+        var legend = cellContent.substring(legendIndex);
+        legend = legend.substring(legend.indexOf('=') + 1, legend.indexOf('"'));
+        //alert(legend);
+        legend = legend.split('|');
+        
+        var chartData = [];
+
+        for(var i = 0; i < data.length; i++){
+            chartData[i] = {label: legend[i], x: i*5, y: parseInt(data[i])};
+        }
+        cell.innerHTML = '<div id="chartContainer"></div>';
+        
+        var chart = new CanvasJS.Chart("chartContainer", {
+            
+            title:{
+                text: "Einkunnir"              
+            },
+            data: [//array of dataSeries              
+                { //dataSeries object
+                    
+                    /*** Change type "column" to "bar", "area", "line" or "pie"***/
+                    type: "column",
+                    dataPoints: chartData
+                }
+            ]
+        });
+        
+        chart.render();
+    }
+}
