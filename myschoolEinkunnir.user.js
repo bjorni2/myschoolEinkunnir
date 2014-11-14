424f34f6ea0307a228da0df16e786905cd3ecb6a
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-11-14 14:14:05 +0000

    First attempt at displaying all graphs

--- myschoolEinkunnir.user.js
+++ myschoolEinkunnir.user.js
@@ -4,26 +4,30 @@
 // @version				0.2
 // @author				Björn Ingi Baldvinsson
 // @description	        Birtir einkunnadreifingu.
-// @include				https://myschool.ru.is/myschool/*verkID*
+// @include				https://myschool.ru.is/myschool/*
 // @require				http://cdnjs.cloudflare.com/ajax/libs/canvasjs/1.4.1/canvas.min.js
 // ==/UserScript==
-var myList = document.getElementsByTagName("h3"); // get all h3 elements
+var myList = document.getElementsByTagName("img"); // get all img elements
+var chartCount = 0;
+//alert(myList.length);
 for(var i = 0; i < myList.length; i++){
-    if(myList[i].textContent  == 'Tölfræði'){
-
-        var cell = myList[i].nextSibling.firstChild.firstChild.firstChild;
+   // alert(myList[i].parentNode.innerHTML);
+    if(myList[i].getAttribute('src').lastIndexOf('system/Skins/ruGraph.asp', 0)  === 0){
+        
+        //var cell = myList[i].nextSibling.firstChild.firstChild.firstChild;
+        var cell = myList[i].parentNode;
         var cellContent = cell.innerHTML;
-
+        
         var dataIndex = cellContent.indexOf('data');
         var data = cellContent.substring(dataIndex);
         data = data.substring(data.indexOf('=') + 1, data.indexOf('&'));
-
+        
         data = data.split('|');
         
         var legendIndex = cellContent.indexOf('legend');
         var legend = cellContent.substring(legendIndex);
         legend = legend.substring(legend.indexOf('=') + 1, legend.indexOf('"'));
-
+        
         legend = legend.split('|');
         
         var selectedIndex = cellContent.indexOf('selected');
@@ -33,16 +37,17 @@ for(var i = 0; i < myList.length; i++){
         
         var chartData = [];
         
-        for(var i = 0; i < data.length; i++){
-            if(i + 1 == selected){
-                chartData[i] = {label: legend[i], y: parseInt(data[i]), x: i, color: "red" };
+        for(var j = 0; j < data.length; j++){
+            if(j + 1 == selected){
+                chartData[j] = {label: legend[j], y: parseInt(data[j]), x: j, color: "red" };
             }else{
-                chartData[i] = {label: legend[i], y: parseInt(data[i]), x: i};
+                chartData[j] = {label: legend[j], y: parseInt(data[j]), x: j};
             }
         }
-        cell.innerHTML = '<div id="chartContainer" style="height: 400px; width: 75%;"></div>';
-        
-        var chart = new CanvasJS.Chart("chartContainer", {
+        cell.innerHTML = '<div id="chartContainer' + i + '" style="height: 400px; width: 75%;"></div>';
+        //alert(i);
+        var charts = [];
+        charts[chartCount] = new CanvasJS.Chart("chartContainer" + i, {
             
             title:{
                 text: "Dreifing einkunna"              
@@ -58,8 +63,6 @@ for(var i = 0; i < myList.length; i++){
             
             data: [            
                 { 
-                    indexLabelFontSize: 32,
-                    indexLabelFontFamily:"Lucida Console" ,
                     /*** Change type "column" to "bar", "area", "line" or "pie"***/
                     type: "column",
                     color: "blue",
@@ -67,7 +70,10 @@ for(var i = 0; i < myList.length; i++){
                 }
             ]
         });
-        
-        chart.render();
+        chartCount++;
     }
+}
+
+for(var i = 0; i < charts.length; i++){
+    charts[i].render();   
 }
